5d59ac5d3f99cef41af248a283369a12509f9170
Philip Valer <pvalerpolanco@mail.valenciacollege.edu>
2026-03-28 23:46:08 -0400

    Modification and creation of files

--- src/MyNav.tsx
+++ src/MyNav.tsx
@@ -1,12 +1,14 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function MyNav() {
   return (
-    <BrowserRouter>
+    /* The basename tells React Router that the app lives at /web-development-2/ */
+    <BrowserRouter basename="/web-development-2">
       <SetNav />
       <Routes>
+        {/* Set the default path to Home */}
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,8 +21,13 @@ function SetNav() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        {/* Changed <l1> to <li> and fixed the About link text */}
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
