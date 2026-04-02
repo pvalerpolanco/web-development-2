c65a87d4ce0b0b7a631a3d298471a6a1bbd0a44c
Philip Valer <pvalerpolanco@mail.valenciacollege.edu>
2026-04-02 11:58:11 -0400

    Products file created

---
+++ src/Products.tsx
@@ -0,0 +1,36 @@
+function Products() {
+  return (
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Product Type</th>
+                <th>Cost</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Apple</td>
+                <td>Fruit</td>
+                <td>2.50</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Pineapple</td>
+                <td>Fruit</td>
+                <td>3.25</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Cucumber</td>
+                <td>Vegetable</td>
+                <td>.50</td>
+            </tr>
+        </tbody>
+    </table>
+  );
+}
+
+export default Products;
