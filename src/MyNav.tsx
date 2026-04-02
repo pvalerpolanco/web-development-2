20948fd9baf8da1060d66e710266c145155bed6e
Philip Valer <pvalerpolanco@mail.valenciacollege.edu>
2026-04-02 14:46:30 -0400

    Products added

--- src/MyNav.tsx
+++ src/MyNav.tsx
@@ -22,11 +22,7 @@ function SetNav() {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li>
-          <a href="https://pvalerpolanco.github.io/products">
-            Products
-          </a>
-        </li>
+        <li><Link to="/products">Products</Link></li>
       </ul>
     </nav>
   );
