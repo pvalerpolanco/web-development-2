0d4cd35c72f38bca22b8a355bad399fbd4d0797f
Philip Valer <pvalerpolanco@mail.valenciacollege.edu>
2026-04-02 12:15:12 -0400

    "Added Products to navigation"

--- src/MyNav.tsx
+++ src/MyNav.tsx
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import About from "./About";
+import Products from "./Products";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function MyNav() {
@@ -7,9 +8,9 @@ function MyNav() {
     <BrowserRouter>
       <SetNav />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
@@ -19,8 +20,9 @@ function SetNav() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
+        <li><Link to="/products">Products</Link></li>
       </ul>
     </nav>
   );
