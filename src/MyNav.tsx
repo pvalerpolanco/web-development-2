973a21e9de8fba1e6cc3c4db985fec7b62ad81c8
Philip Valer <pvalerpolanco@mail.valenciacollege.edu>
2026-04-04 19:06:45 -0400

    Form and Products

--- src/MyNav.tsx
+++ src/MyNav.tsx
@@ -2,11 +2,11 @@ import Home from "./Home";
 import About from "./About";
 import Products from "./Products";
 import MyForm from "./MyForm";
-import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function MyNav() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <SetNav />
       <Routes>
         <Route index element={<Home />} />
@@ -15,7 +15,7 @@ function MyNav() {
         <Route path="products" element={<Products />} />
         <Route path="form" element={<MyForm />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
@@ -23,10 +23,10 @@ function SetNav() {
   return (
     <nav>
       <ul>
-        <li><Link to="#/">Home</Link></li>
-        <li><Link to="#/about">About</Link></li>
-        <li><Link to="#/products">Products</Link></li>
-        <li><Link to="#/form">Form</Link></li>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/form">Form</Link></li>
       </ul>
     </nav>
   );
