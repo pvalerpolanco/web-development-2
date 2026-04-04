1439feb84db298e5557a81f94e8042ce6a3c1608
Philip Valer <pvalerpolanco@mail.valenciacollege.edu>
2026-04-04 17:56:17 -0400

    React project

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,25 +1,35 @@
 function Products() {
   return (
-    <div className="Products">
-      <h1>Products</h1>
-      <ul className="product-list">
-        <li className="product-item">
-          <span className="product-name">Apple</span>
-          <span className="product-type">Fruit</span>
-          <span className="product-cost">$2.50</span>
-        </li>
-        <li className="product-item">
-          <span className="product-name">Pineapple</span>
-          <span className="product-type">Fruit</span>
-          <span className="product-cost">$3.25</span>
-        </li>
-        <li className="product-item">
-          <span className="product-name">Cucumber</span>
-          <span className="product-type">Vegetable</span>
-          <span className="product-cost">$0.50</span>
-        </li>
-      </ul>
-    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Product Type</th>
+                <th>Cost</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Apple</td>
+                <td>Fruit</td>
+                <td>2.50</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Pineapple</td>
+                <td>Fruit</td>
+                <td>3.25</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Cucumber</td>
+                <td>Vegetable</td>
+                <td>0.50</td>
+            </tr>
+        </tbody>
+    </table>
   );
 }
 
